52f4a62567bac7f3830102a3996f29c9c1679d29
ToEzBit <jirayut52328172@hotmail.com>
2024-02-25 14:13:15 +0700

    signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
-    const hash = await argon.hash(dto.password);
+    try {
+      const hash = await argon.hash(dto.password);
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hash,
-      },
-    });
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
 
-    delete user.hash;
-    return user;
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Creatential is taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
