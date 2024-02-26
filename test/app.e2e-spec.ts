a649cec9bd12eea450798e831f9d712267d4d42b
ToEzBit <jirayut52328172@hotmail.com>
2024-02-26 16:28:19 +0700

    prisma teardown

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -16,10 +18,12 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+
+    prisma = app.get(PrismaService);
+    prisma.cleanDb();
   });
 
   afterAll(() => {
     app.close();
   });
-  it.todo('should pass');
 });
