5e29787fc69612864fe9bc1b53c78062820bc509
ToEzBit <jirayut52328172@hotmail.com>
2024-03-19 13:43:50 +0700

    kkkkkkkkkkkkk

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('kuy');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
